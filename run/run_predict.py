66d8c59ce325d46070c7c31ae98adbefea9f2f02
ayanas <ayana.shenhav@gmail.com>
2023-08-02 07:52:11 +0300

    pathfix

--- run/run_predict.py
+++ run/run_predict.py
@@ -12,7 +12,7 @@ def asr_predict(ckpt_dir):
     model_path = os.path.join(ckpt_dir, 'best.ckpt')
     model = ASRModelLightening.load_from_checkpoint(model_path, config=config)
     OmegaConf.update(config, "data.dataloader.validation_batch_size", 200)
-    data_module = ASRDataModule(config.data)
+    data_module = ASRDataModule(config)
     data_module.setup("predict")
     for batch in data_module.val_dataloader():
         print(batch.keys())
