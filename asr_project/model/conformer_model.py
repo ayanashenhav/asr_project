4c7b07f7338ef1d9c114acad50034a506bbc9d1d
ayanas <ayana.shenhav@gmail.com>
2023-08-02 03:22:07 +0300

    make conformer word

--- asr_project/model/conformer_model.py
+++ asr_project/model/conformer_model.py
@@ -3,20 +3,21 @@ import torch
 from .base_model import BaseModel
 from typing import Dict
 from torchaudio.models import Conformer
+from torch import nn
 
 
 class ConformerWrapper(BaseModel):
     def __init__(self, config):
         super().__init__()
         self.config = config
-        self.model = Conformer(**config.architecture)
+        self.conformer = Conformer(**config.conformer_architecture)
+        self.linear = nn.Linear(config.conformer_architecture['input_dim'], config['out_channels'])
 
     def inference(self, batch: Dict) -> Dict:
-        self.model.eval()
-        with torch.no_grad():
-            return self.forward(batch)
+        return self.forward(batch)
 
     def forward(self, batch: Dict) -> Dict:
-        preds, preds_len = self.model.forward(**{'input': batch['input'].permute([1, 0, 2]),
+        preds, preds_len = self.conformer.forward(**{'input': batch['input'].permute([1, 0, 2]),
                                                  'lengths': batch['input_lengths']})
+        preds = self.linear.forward(preds)
         return dict(preds=preds.permute([1, 0, 2]), preds_len=preds_len)
